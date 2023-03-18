b75c094f4b07a9fe1b072cddff846d5aecacc84c
camiloricoe <camilo.rico.echeverry@gmail.com>
2023-03-18 15:53:19 -0500

    useCatFact custom hook

--- Midu/React1/prueba-tecnica-junior/src/App.jsx
+++ Midu/React1/prueba-tecnica-junior/src/App.jsx
@@ -7,23 +7,26 @@ const CAT_ENDPOINT_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_URL = `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
-
-export function App() {
+function useCatFact() {
     const [fact, setFact] = useState()
-    const { imageURL } = useCatImage({ fact }) //custom hook 
 
-
-    const handleClick = () => {
+    const updateFact = () => {
         getRandomFact().then(setFact)
     }
 
-    useEffect(() => {
-        getRandomFact().then(setFact)
-    }, [])
+    return { fact, updateFact }
 
+}
 
+export function App() {
+    const { fact, updateFact } = useCatFact()
+    const { imageURL } = useCatImage({ fact }) //custom hook 
 
+    const handleClick = () => {
+        updateFact()
+    }
 
+    useEffect(updateFact, [])
 
 
     return (
